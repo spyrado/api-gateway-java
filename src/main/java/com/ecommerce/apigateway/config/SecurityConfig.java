20f3b42b711e4b99dcb5012c924f252d0f52a48a
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2026-05-25 23:43:24 -0300

    feat: arrumando comunicação do gateway e dando bypass temporario de segurança

---
+++ src/main/java/com/ecommerce/apigateway/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.ecommerce.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+    return http
+        .csrf(csrf -> csrf.disable())
+        .authorizeExchange(auth -> auth
+            .anyExchange().permitAll() // ← libera tudo por enquanto
+        )
+        .build();
+  }
+}
